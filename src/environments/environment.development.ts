01bdb58c7ce24f95a3cc497bbbf24ad290bf26cc
Gabriel Evaristo <gabrielousadia12@gmail.com>
2024-10-22 12:54:57 -0300

    Date update

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -5,15 +5,15 @@ export const environment = {
     tarefas: [
         new Task("Lavar o carro", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur auctor, elit id imperdiet venenatis, nunc augue sollicitudin justo, sed sollicitudin enim leo a eros. Suspendisse accumsan lacus nec leo semper", 
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur auctor, elit id imperdiet venenatis, nunc augue sollicitudin justo, sed sollicitudin enim leo a eros. Suspendisse accumsan lacus nec leo semper", 
-            new Date("01/01/2000"), new Date("01/01/2000"), Status.pendente),
+            new Date("01/01/2000"), new Date("01/01/2024"), Status.pendente),
         new Task("Passear com cachorra", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur auctor, elit id imperdiet venenatis, nunc augue sollicitudin justo, sed sollicitudin enim leo a eros. Suspendisse accumsan lacus nec leo semper", 
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur auctor, elit id imperdiet venenatis, nunc augue sollicitudin justo, sed sollicitudin enim leo a eros. Suspendisse accumsan lacus nec leo semper", 
-            new Date("01/01/2000"), new Date("01/02/2000"), Status.iniciado),
+            new Date("01/01/2000"), new Date("01/02/2024"), Status.iniciado),
         new Task("Trabalho de faculdade", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur auctor, elit id imperdiet venenatis, nunc augue sollicitudin justo, sed sollicitudin enim leo a eros. Suspendisse accumsan lacus nec leo semper", 
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur auctor, elit id imperdiet venenatis, nunc augue sollicitudin justo, sed sollicitudin enim leo a eros. Suspendisse accumsan lacus nec leo semper", 
-            new Date("01/01/2000"), new Date("01/03/2000"), Status.concluido),
+            new Date("01/01/2000"), new Date("01/03/2024"), Status.concluido),
         new Task("Passar pano nos moveis", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur auctor, elit id imperdiet venenatis, nunc augue sollicitudin justo, sed sollicitudin enim leo a eros. Suspendisse accumsan lacus nec leo semper", 
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur auctor, elit id imperdiet venenatis, nunc augue sollicitudin justo, sed sollicitudin enim leo a eros. Suspendisse accumsan lacus nec leo semper", 
-            new Date("01/01/2000"), new Date("01/04/2000"), Status.pendente),
+            new Date("01/01/2000"), new Date("01/04/2024"), Status.pendente),
     ]
 };
