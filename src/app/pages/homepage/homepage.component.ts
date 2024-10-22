124b2c2573053ab44914482d3db146a3c05d02d7
Gabriel Evaristo <gabrielousadia12@gmail.com>
2024-10-22 10:39:50 -0300

    Sistema de filtro adicionado

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, input } from '@angular/core';
 import { TaskcardComponent } from '../../components/taskcard/taskcard.component';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
@@ -27,25 +27,63 @@ import {MatSelectModule} from '@angular/material/select';
     MatSelectModule],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  changeDetection: ChangeDetectionStrategy.Default,
 })
 export class HomepageComponent {
   private taskList = environment.tarefas
-  protected filteredTaskList = this.taskList;
-  protected filterStatus!:Status;
+  protected filteredTaskList = [...this.taskList];
   protected filterInput:string = "title";
-  protected statusInstance = Status
-
-
+  protected statusInstance = Status;
+  protected selectionValue!: Status
 
   ngOnInit(){
-    console.log(this.filteredTaskList)
+    this.filteredTaskList.sort((a,b) => b.status.localeCompare(a.status))
   }
 
-  fitlerByStatus(status: Status){
-    this.filterStatus
+  ngDoCheck(){
+    this.filteredTaskList = this.filteredTaskList.sort((a,b) =>{ 
+      return b.status.localeCompare(a.status)
+    })
   }
+
   changeFilterType(filterType: string){
+    this.filteredTaskList = [...this.taskList]
     this.filterInput = filterType
   }
+
+  filter(event: any){
+    switch(this.filterInput){
+      case("title"):
+        var inputValue = event.target.value.toLowerCase().trim();
+        if(!inputValue){
+          this.filteredTaskList = [...this.taskList]
+        }else{
+          this.filteredTaskList = this.taskList.filter(task => {
+            return task.title.toLowerCase().includes(inputValue)
+          })
+        }
+        break;
+      case("deadLine"):
+        var inputValue = event.target.value;
+        if(!inputValue){
+          this.filteredTaskList = [...this.taskList]
+        }else{
+          this.filteredTaskList = this.filteredTaskList.filter((task) => {
+            return task.deadLineDate == inputValue
+          })
+        }
+        break;
+      case("status"):
+        if(!this.selectionValue){
+          this.filteredTaskList = [...this.taskList]
+        }else{
+          this.filteredTaskList = this.filteredTaskList.filter((task) => {
+            return task.status == this.selectionValue
+          })
+        }
+        break;
+        
+    }
+  }
+
 }
